8d9ad6a747976047b8292b606a5e765ce2fd9989
Cuauhtli Fernando <cuauh_fer_bh@hotmail.com>
2018-04-17 19:53:49 -0500

    Formulario mejorado de produccion, ya publica en base de datos, detalles de botones y linea

--- int_produccion.php
+++ int_produccion.php
@@ -35,13 +35,6 @@
 	if(isset($_SESSION['username'])){
 		$conexion = mysqli_connect("localhost", "Fernando", "Cuauhtli", "b17_21017364_CuerpoAcademico");
 		$usuario = $_SESSION['username'];
-
-		if(isset($_POST['agregar'])){
-
-			$sql = "INSERT INTO usuario(codigo, username, password, nivel, nombre, apellidop, apellidom, email, telefono, division, escolaridad) VALUES('$codigo', '$username', '$password', '$nivel', '$nombre', '$apep', '$apem', '$correo', '$telefono', '$division', '$escolaridad')";
-			$resultado = mysqli_query($conexion, $sql);
-			
-		}
 ?>
 	<body>
 		<header>
@@ -72,13 +65,61 @@
 		<section id="banner">
 			<br><br><br><br>
 				<div class="container">
-					<?php  
-						if(isset($_POST['comenzar'])){
+					<form method="post">
 
+						<!-- Tabla para ver el tipo de publicacion o registro-->
+						<table class="table table-dark table-bordered table-striped table-hover">
+							<tr>
+								<td colspan="1">Tipo de Publicacion</td>
+								<td colspan="2"><select class="form-control custom-select" type="select" name="tipo" id="tipo">
+									<option <?php if(isset($_POST['tipo']) && $_POST['tipo']=="1"){echo "selected";}?> value="1">Articulo</option>
+									<option <?php if(isset($_POST['tipo']) && $_POST['tipo']=="2"){echo "selected";}?> value="2">Informe Tecnico</option>
+									<option <?php if(isset($_POST['tipo']) && $_POST['tipo']=="3"){echo "selected";}?> value="3">Manual</option>
+									<option <?php if(isset($_POST['tipo']) && $_POST['tipo']=="4"){echo "selected";}?> value="4">Libro</option>
+								</select></td>
+								<td><input class="btn btn-success" type="submit" name="comenzar" value="Comenzar"></td>
+							</tr>
+						</table>
+					<?php 
+						//If para comenzar con la seleccion
+						if(isset($_POST['comenzar']) || isset($_POST['agregar'])){
+
+							//Cada if es un tipo distinto de publicacion
 							if($_POST['tipo'] == "1"){
+								$persona = $_SESSION['persona'];
+								$tipo = $_POST['tipo'];
+
+								if(isset($_POST['agregar'])){
+									$nombre = $_POST['nombre'];
+									$autor = $persona['codigo'];
+									$fecha = $_POST['fecha'];
+									if($_POST['borrador']){
+										$borrador = true;
+									}
+									else{
+										$borrador = false;
+									}
+									$aprobacion = false;
+									$publicacion = $tipo;
+									$sql = "INSERT INTO produccion(nombre, autor, fecha, borrador, aprobacion, tipoPublicacion) VALUES('$nombre', '$autor', '$fecha', '$borrador', '$aprobacion', '$publicacion')";
+									$resultado = mysqli_query($conexion, $sql);
+
+									$sql = "SELECT *  FROM produccion WHERE nombre LIKE '$nombre' AND autor = '$autor'";
+									$resultado = mysqli_query($conexion, $sql);
+									$reg = mysqli_fetch_array($resultado);
+
+									$idProduccion = $reg['id'];
+									$revista = $_POST['revista'];
+									$paginas = "Página ".$_POST['pag1']." hasta la página ".$_POST['pag2'];
+									$linea = $_POST['linea'];
+									$issn = $_POST['issn'];
+
+									$sql = "INSERT INTO articulo(revista, paginas, linea, issn, idProduccion) VALUES('$revista', '$paginas', '$linea', '$issn', '$idProduccion')";
+									$resultado = mysqli_query($conexion, $sql);
+									echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";
+								}
 								?>
 									<div class="container"></div>
-									<form method="post">
 										<table class="table table-dark table-bordered table-striped table-hover">
 											
 											<tr>
@@ -88,11 +129,11 @@
 											<tr>
 												<td colspan="1">Autor</td>
 												<td colspan="3"><?php 
-													$persona = $_SESSION['persona']; echo $persona['nombre'];?></td>
+													 echo $persona['nombre'];?></td>
 											</tr>
 											<tr>
 												<td>Fecha</td>
-												<td><input class="form-control" type="date" name="fecha" required></td>
+												<td><input class="form-control" type="date" name="fecha"></td>
 												<td colspan="2">
 													<div class="checkbox input-group-text">
 														<label>
@@ -102,7 +143,7 @@
 												</td>
 											</tr>
 											<tr>
-												<td colspan="1">Revista</td><td colspan="3"><input class="form-control" type="text" name="revista" required></td>
+												<td colspan="1">Revista</td><td colspan="3"><input class="form-control" type="text" name="revista"></td>
 											</tr>
 											<tr>
 												<td colspan="1">Paginas</td>
@@ -123,7 +164,7 @@
 												<td colspan="1">Linea</td><td colspan="3"><input class="form-control" type="text" name="linea" required></td>
 											</tr>
 											<tr>
-												<td colspan="1">ISSN</td><td colspan="3"><input class="form-control" type="text" name="issn" required></td>
+												<td colspan="1">ISSN</td><td colspan="3"><input class="form-control" type="text" name="issn"></td>
 											</tr>
 											<tr>
 												<td colspan="4"></td>
@@ -134,10 +175,38 @@
 												<td colspan="2"><input class="btn btn-success" type="submit" name="cancelar" value="Cancelar"></td>
 											</tr>
 										</table>
-									</form>
 								<?php
 							}
 							else if($_POST['tipo'] == "2"){
+								$persona = $_SESSION['persona'];
+								$tipo = $_POST['tipo'];
+
+								if(isset($_POST['agregar'])){
+									$nombre = $_POST['nombre'];
+									$autor = $persona['codigo'];
+									$fecha = $_POST['fecha'];
+									if($_POST['borrador']){
+										$borrador = true;
+									}
+									else{
+										$borrador = false;
+									}
+									$aprobacion = false;
+									$publicacion = $tipo;
+									$sql = "INSERT INTO produccion(nombre, autor, fecha, borrador, aprobacion, tipoPublicacion) VALUES('$nombre', '$autor', '$fecha', '$borrador', '$aprobacion', '$publicacion')";
+									$resultado = mysqli_query($conexion, $sql);
+
+									$sql = "SELECT *  FROM produccion WHERE nombre LIKE '$nombre' AND autor = '$autor'";
+									$resultado = mysqli_query($conexion, $sql);
+									$reg = mysqli_fetch_array($resultado);
+
+									$idProduccion = $reg['id'];
+									$dependencia = $_POST['dependencia'];
+
+									$sql = "INSERT INTO informetec(dependencia, idProduccion) VALUES('$dependencia', '$idProduccion')";
+									$resultado = mysqli_query($conexion, $sql);
+									echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";
+								}
 								?>
 									<form method="post">
 											<table class="table table-dark table-bordered table-striped table-hover">
@@ -149,15 +218,15 @@
 												<tr>
 													<td colspan="1">Autor</td>
 													<td colspan="3"><?php 
-														$persona = $_SESSION['persona']; echo $persona['nombre'];?></td>
+														echo $persona['nombre'];?></td>
 												</tr>
 												<tr>
 													<td>Fecha</td>
-													<td><input class="form-control" type="date" name="fecha" required></td>
+													<td><input class="form-control" type="date" name="fecha"></td>
 													<td colspan="2">
 														<div class="checkbox input-group-text">
 															<label>
-																<input type="checkbox" aria-label="Checkbox for following text input" name="borrador"> Borrador
+																<input type="checkbox" name="borrador"> Borrador
 															</label>
 														</div>
 													</td>
@@ -178,6 +247,35 @@
 									<?php
 							}
 							else if($_POST['tipo'] == "3"){
+								$persona = $_SESSION['persona'];
+								$tipo = $_POST['tipo'];
+
+								if(isset($_POST['agregar'])){
+									$nombre = $_POST['nombre'];
+									$autor = $persona['codigo'];
+									$fecha = $_POST['fecha'];
+									if($_POST['borrador']){
+										$borrador = true;
+									}
+									else{
+										$borrador = false;
+									}
+									$aprobacion = false;
+									$publicacion = $tipo;
+									$sql = "INSERT INTO produccion(nombre, autor, fecha, borrador, aprobacion, tipoPublicacion) VALUES('$nombre', '$autor', '$fecha', '$borrador', '$aprobacion', '$publicacion')";
+									$resultado = mysqli_query($conexion, $sql);
+
+									$sql = "SELECT *  FROM produccion WHERE nombre LIKE '$nombre' AND autor = '$autor'";
+									$resultado = mysqli_query($conexion, $sql);
+									$reg = mysqli_fetch_array($resultado);
+
+									$idProduccion = $reg['id'];
+									$registro = $_POST['registro'];
+
+									$sql = "INSERT INTO manual(registro, idProduccion) VALUES('$registro', '$idProduccion')";
+									$resultado = mysqli_query($conexion, $sql);
+									echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";
+								}
 								?>
 									<form method="post">
 											<table class="table table-dark table-bordered table-striped table-hover">
@@ -189,11 +287,11 @@
 												<tr>
 													<td colspan="1">Autor</td>
 													<td colspan="3"><?php 
-														$persona = $_SESSION['persona']; echo $persona['nombre'];?></td>
+														echo $persona['nombre'];?></td>
 												</tr>
 												<tr>
 													<td>Fecha</td>
-													<td><input class="form-control" type="date" name="fecha" required></td>
+													<td><input class="form-control" type="date" name="fecha"></td>
 													<td colspan="2">
 														<div class="checkbox input-group-text">
 															<label>
@@ -203,7 +301,7 @@
 													</td>
 												</tr>
 												<tr>
-													<td colspan="1">Registro</td><td colspan="3"><input class="form-control" type="text" name="registro" required></td>
+													<td colspan="1">Registro</td><td colspan="3"><input class="form-control" type="text" name="registro"></td>
 												</tr>
 												<tr>
 													<td colspan="4"></td>
@@ -218,6 +316,38 @@
 									<?php
 							}
 							else if($_POST['tipo'] == "4"){
+								$persona = $_SESSION['persona'];
+								$tipo = $_POST['tipo'];
+
+								if(isset($_POST['agregar'])){
+									$nombre = $_POST['nombre'];
+									$autor = $persona['codigo'];
+									$fecha = $_POST['fecha'];
+									if($_POST['borrador']){
+										$borrador = true;
+									}
+									else{
+										$borrador = false;
+									}
+									$aprobacion = false;
+									$publicacion = $tipo;
+									$sql = "INSERT INTO produccion(nombre, autor, fecha, borrador, aprobacion, tipoPublicacion) VALUES('$nombre', '$autor', '$fecha', '$borrador', '$aprobacion', '$publicacion')";
+									$resultado = mysqli_query($conexion, $sql);
+
+									$sql = "SELECT *  FROM produccion WHERE nombre LIKE '$nombre' AND autor = '$autor'";
+									$resultado = mysqli_query($conexion, $sql);
+									$reg = mysqli_fetch_array($resultado);
+
+									$idProduccion = $reg['id'];
+									$editorial = $_POST['editorial'];
+									$paginas = "Página ".$_POST['pag1']." hasta la página ".$_POST['pag2'];
+									$linea = $_POST['linea'];
+									$isbn = $_POST['isbn'];
+
+									$sql = "INSERT INTO libro(paginas, editorial, linea, isbn, idProduccion) VALUES('$paginas', '$editorial', '$linea', '$isbn', '$idProduccion')";
+									$resultado = mysqli_query($conexion, $sql);
+									echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";
+								}
 								?>
 									<form method="post">
 											<table class="table table-dark table-bordered table-striped table-hover">
@@ -261,7 +391,7 @@
 													</td>
 												</tr>
 												<tr>
-													<td colspan="1">Linea</td><td colspan="3"><input class="form-control" type="text" name="Linea" required></td>
+													<td colspan="1">Linea</td><td colspan="3"><input class="form-control" type="text" name="linea" required></td>
 												</tr>
 												<tr>
 													<td colspan="1">Editorial</td><td colspan="3"><input class="form-control" type="text" name="editorial" required></td>
@@ -279,25 +409,12 @@
 									<?php
 							}
 						}
-						else{
-							?>
-								<table class="table table-dark table-bordered table-striped table-hover">
-									<tr>
-										<form method="post">
-											<td colspan="1">Tipo de Publicacion</td>
-											<td colspan="2"><select class="form-control custom-select" type="select" name="tipo" id="tipo">
-												<option value="1">Articulo</option>
-												<option value="2">Informe Tecnico</option>
-												<option value="3">Manual</option>
-												<option value="4">Libro</option>
-											</select></td>
-											<td><input class="btn btn-success" type="submit" name="comenzar" value="Comenzar"></td>
-										</form>
-									</tr>
-								</table>
-							<?php
+						else if(isset($_POST['cancelar'])){
+							//Funcion del boton de cancelar
+							echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";
 						}
 					?>
+					</form>
 				</div>
 		</section>
 		<script src="js/jquery.js"></script>
@@ -305,7 +422,7 @@
 	</body>
 <?php  
 	}
-	else{
+	else{//Else de sesion
 		header('Location: login.php');
 	}
 ?>
